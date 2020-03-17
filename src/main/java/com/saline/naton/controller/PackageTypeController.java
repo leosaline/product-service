65374d07006b7fb81778bb899bfcb152ae80d919
leosaline <leosaline@gmail.com>
2020-03-17 20:58:51 -0300

    add host to allow cross request

--- src/main/java/com/saline/naton/controller/PackageTypeController.java
+++ src/main/java/com/saline/naton/controller/PackageTypeController.java
@@ -20,7 +20,7 @@ public class PackageTypeController {
 
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Return list of products") })
 	@RequestMapping(value = "/packagetypes", method = RequestMethod.GET, produces = "application/json")
-	@CrossOrigin(origins = "http://localhost:8080")
+	@CrossOrigin(origins = {"http://localhost:8080", " http://natonfrontend:8080"})
 	@ResponseBody
 	public ResponseEntity<Collection<PackageTypeEnum>> listPackageType() {
 		return ResponseEntity.ok(Arrays.asList(PackageTypeEnum.values()));
